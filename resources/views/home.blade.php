c85f1542a9901574f57b4f27d0d3649b5309135e
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-04-03 00:31:02 -0600

    Create Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue Test</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app">
+        <home></home> <!-- narnaja -->
+    </div>
+</body>
+</html>
