97686ecfde3eae3b476badcc522d1b2c6e4d68e5
FabianMoraeles <fabimoradav2004@gmail.com>
2025-04-05 10:49:02 -0600

    Ingreso roles predeterminados, ADMIN, inventario, secretaria, docente, estudiante.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido', 100)->nullable(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->date('fecha_registro')->nullable(false);
+            $table->date('fecha_nacimiento')->nullable(false);
             $table->timestamps();
         });
 
