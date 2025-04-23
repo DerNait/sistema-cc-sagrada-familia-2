6ddb4d147400ed0b37fc26bc035a5bc570941435
kevin-softlogic <kevin@softlogic.com.gt>
2025-04-23 17:55:34 -0600

    button now generates planilla

--- resources/js/components/Empleados.vue
+++ resources/js/components/Empleados.vue
@@ -1,9 +1,21 @@
 <template>
-    <div class="container mt-5">
-      <h1>Lista de Empleados</h1>
-      <button class="btn btn-primary mt-3">
-        <i class="bi bi-download"></i> Descargar Plantilla
-      </button>
-    </div>
-  </template>
+	<div class="container mt-5">
+		<h1>Lista de Empleados</h1>
+		<button 
+			class="btn btn-primary mt-3"
+			@click="descargarPlanilla"
+		>
+			<i class="bi bi-download"></i> Descargar Plantilla
+		</button>
+	</div>
+</template>
   
+<script setup>
+
+function descargarPlanilla() {
+	console.log("Descargando planilla...");
+
+	window.location.href = "/empleados/planilla"
+}
+
+</script>
