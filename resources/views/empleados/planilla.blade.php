45d0da5693166fd5755d1097884d0971f9f40c0b
kevin-softlogic <kevin@softlogic.com.gt>
2025-04-23 16:22:19 -0600

    PDF to download implemented

---
+++ resources/views/empleados/planilla.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Planilla</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app">
+        <h1>ESTA SERA LA PLANILLA</h1>
+    </div>
+</body>
+</html>
