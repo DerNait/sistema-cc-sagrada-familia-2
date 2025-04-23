96c46e0394bd515e29356e22cb4cd5d823812bf1
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-04-23 12:26:00 -0600

    Creacion de Empleados

---
+++ resources/js/components/Empleados.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container mt-5">
+      <h1>Lista de Empleados</h1>
+      <button class="btn btn-primary mt-3">
+        <i class="bi bi-download"></i> Descargar Plantilla
+      </button>
+    </div>
+  </template>
+  
---
+++ resources/views/empleados.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Empleados</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app">
+        <Empleados></Empleados> 
+    </div>
+</body>
+</html>
