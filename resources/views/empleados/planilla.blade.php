ff8f96c80abdde353ba0c0915cffc27b014ef1c7
kevin-softlogic <kevin@softlogic.com.gt>
2025-04-23 17:52:29 -0600

    add planilla PDF format

--- resources/views/empleados/planilla.blade.php
+++ resources/views/empleados/planilla.blade.php
@@ -2,12 +2,73 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Planilla</title>
-    @vite('resources/js/app.js')
+    <title>Planilla de Salarios - {{ $periodo->mes }}/{{ $periodo->anio }}</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 12px;
+        }
+        h2 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            font-size: 12px;
+        }
+        thead {
+            background-color: #f0f0f0;
+        }
+        th, td {
+            border: 1px solid #333;
+            padding: 5px;
+            text-align: left;
+        }
+        .text-right {
+            text-align: right;
+        }
+    </style>
 </head>
 <body>
-    <div id="app">
-        <h1>ESTA SERA LA PLANILLA</h1>
-    </div>
+
+    <h2>Planilla de Salarios - {{ str_pad($periodo->mes, 2, '0', STR_PAD_LEFT) }}/{{ $periodo->anio }}</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Nombre</th>
+                <th>DPI</th>
+                <th>Cargo</th>
+                <th>Fecha de Ingreso</th>
+                <th>Cuenta</th>
+                <th>Banco</th>
+                <th class="text-right">Salario Base</th>
+                <th class="text-right">Bonificaciones</th>
+                <th class="text-right">Descuentos</th>
+                <th class="text-right">Total a Depositar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $contador = 1; @endphp
+            @foreach ($datosPlanilla as $empleado)
+                <tr>
+                    <td>{{ $contador++ }}</td>
+                    <td>{{ $empleado['name'] }}</td>
+                    <td>{{ $empleado['dpi'] }}</td>
+                    <td>{{ $empleado['cargo'] }}</td>
+                    <td>{{ \Carbon\Carbon::parse($empleado['fecha_ingreso'])->format('d/m/Y') }}</td>
+                    <td>{{ $empleado['cuenta'] }}</td>
+                    <td>{{ $empleado['banco'] }}</td>
+                    <td class="text-right">Q{{ number_format($empleado['salario_base'], 2) }}</td>
+                    <td class="text-right">Q{{ number_format($empleado['bonificaciones'], 2) }}</td>
+                    <td class="text-right">Q{{ number_format($empleado['descuentos'], 2) }}</td>
+                    <td class="text-right">Q{{ number_format($empleado['total'], 2) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
 </body>
 </html>
