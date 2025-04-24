7fd15aa21dc172f39e652b108a4c9a5ff6568c47
Erick <eagi578@gmail.com>
2025-04-23 20:37:50 -0600

    Refactor EdicionUsuarios component to display employee list with a structured table

--- resources/js/components/EdicionUsuarios.vue
+++ resources/js/components/EdicionUsuarios.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        <h1>Edición de Usuarios</h1>
+    <div class="container py-5">
+        <h2 class="mb-4 text-center">Lista de Empleados</h2>
+        <div class="table-responsive">
+            <table class="table table-bordered align-middle text-center">
+                <thead class="table-light">
+                    <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Correo</th>
+                    <th>Rol</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Salario</th>
+                    <th>Fecha Registro</th>
+                    <th>Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
