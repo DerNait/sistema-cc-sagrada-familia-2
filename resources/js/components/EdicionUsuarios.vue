cbffe3c16be0a8dcc580b7e842fa03ba363cb795
Erick <eagi578@gmail.com>
2025-04-23 20:45:53 -0600

    Enhance EdicionUsuarios component to include role selection and additional employee fields in the table

--- resources/js/components/EdicionUsuarios.vue
+++ resources/js/components/EdicionUsuarios.vue
@@ -16,9 +16,64 @@
                     </tr>
                 </thead>
                 <tbody>
-
+                    <tr v-for="(empleado, index) in empleados" :key="index">
+                    <td><input type="text" class="form-control border-success" v-model="empleado.nombre" /></td>
+                    <td><input type="text" class="form-control border-success" v-model="empleado.apellido" /></td>
+                    <td><input type="email" class="form-control border-success" v-model="empleado.correo" /></td>
+                    <td><Multiselect v-model="empleado.rol" :options="roles" placeholder="Rol" label="name" track-by="name" class="multiselect"/></td>
+                    <td><input type="date" class="form-control border-success" v-model="empleado.fechaNacimiento" /></td>
+                    <td><input type="number" class="form-control border-success" v-model="empleado.salario" /></td>
+                    <td><input type="date" class="form-control border-success" v-model="empleado.fechaRegistro" /></td>
+                    <td>
+                        <button class="btn btn-success btn-sm">Guardar</button>
+                    </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </template>
+
+<script setup>
+    import { reactive } from 'vue'
+    import Multiselect from 'vue-multiselect'
+    
+    const roles = [
+        { name: 'Administracion' },
+        { name: 'Inventario' },
+        { name: 'Secretaria' },
+        { name: 'Docente' },
+        { name: 'Estudiante' }
+    ]
+    
+    // Datos de prueba
+    const empleados = reactive([
+        {
+        nombre: 'Nombre 1',
+        apellido: 'Apellido 1',
+        correo: 'ejemplocorreo1@gmail.edu.gt',
+        rol: roles[3],
+        fechaNacimiento: '1990-01-01',
+        salario: 3500,
+        fechaRegistro: '2025-01-01'
+        },
+        {
+        nombre: 'Nombre 2',
+        apellido: 'Apellido 2',
+        correo: 'ejemplocorreo2@gmail.edu.gt',
+        rol: roles[2],
+        fechaNacimiento: '1992-02-02',
+        salario: 4000,
+        fechaRegistro: '2025-02-02'
+        },
+    ])
+</script>
+
+<style scoped>
+    .multiselect {
+        border: 1px solid #198754;
+        border-radius: 4px;
+        padding: 0.375rem 0.75rem;
+        min-height: 38px;
+    }
+</style>
