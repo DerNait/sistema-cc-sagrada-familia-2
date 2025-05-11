d6aad4eaf88cddefe3cd315358e073f1b8011b6e
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-05-10 23:42:09 -0600

    add nav module to navigate to different modules

---
+++ app/Navigation/NavModule.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Navigation;
+
+class NavModule
+{
+    public const CRUD  = ['create', 'read', 'update', 'delete'];
+    public const READ  = ['read'];
+    public const NONE  = [];
+
+    public string  $label;
+    public string  $key;
+    public int     $order;
+    public ?string $icon;
+    public ?string $parentKey;
+    public array   $actions;
+    public string  $routeName;
+    public string  $menuAction;
+
+    public function __construct(
+        string  $label,
+        string  $key,
+        int     $order,
+        ?string $icon       = null,
+        ?string $parentKey  = null,
+        array   $actions    = self::CRUD,
+        string  $menuAction = 'read',
+        ?string $routeName  = null
+    ) {
+        $this->label      = $label;
+        $this->key        = $key;
+        $this->order      = $order;
+        $this->icon       = $icon;
+        $this->parentKey  = $parentKey;
+        $this->actions    = $actions;
+        $this->menuAction = $menuAction;
+
+        if ($routeName) {
+            // si explícitamente me pasaron uno, lo uso
+            $this->routeName = $routeName;
+        } else {
+            // sino: si hay parent, lo antepongo
+            if ($parentKey) {
+                $this->routeName = "{$parentKey}.{$key}.index";
+            } else {
+                $this->routeName = "{$key}.index";
+            }
+        }
+    }
+}
