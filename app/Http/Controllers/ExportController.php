8506e4265331b94aed27f8a3462e8280e34cb3a3
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-05-14 21:15:12 -0600

    Cases to call download xlsx

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\EmpleadosExport; // Asegúrate de importar la clase EmpleadosExport
-
+use App\Exports\ProductoExport;
 class ExportController extends Controller
 {
-    public function export()
+    public function export(Request $request)
     {
-        // Obtener los empleados desde la base de datos y exportarlos a un archivo Excel
-        return Excel::download(new EmpleadosExport, 'empleados.xlsx');
+      switch ($request->tipo){  
+        case 1:
+            return Excel::download(new EmpleadosExport, 'empleados.xlsx');// Obtener los empleados desde la base de datos y exportarlos a un archivo Excel
+        case 2:
+            return Excel::download(new ProductoExport, 'productos.xlsx');
+        default:
+            return response()->json(['error'=>'Tipo de exportacion no valido'], 400);
+      }
     }
 }
     
