5b3acd79f547a9d9dc61cc1dc760db7d473b92f9
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2025-05-14 23:55:16 -0600

    Change color

--- resources/js/components/Inventario.vue
+++ resources/js/components/Inventario.vue
@@ -102,13 +102,13 @@
           <div class="col-12 d-flex justify-content-end gap-2">
             <button
               @click="resetFilters"
-              class="btn btn-outline-secondary"
+              class="btn btn-danger"
             >
               Limpiar Filtros
             </button>
             <button
               @click="aplicarFiltros"
-              class="btn btn-primary"
+              class="btn btn-success"
             >
               Aplicar Filtros
             </button>
@@ -155,7 +155,7 @@
           
         
           <button
-            class="btn btn-primary mt-4"
+            class="btn btn-success mt-4"
             @click="exportarEmpleados"
             :disabled="isExporting"
           >
