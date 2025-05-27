d0a0eb13cae1520d26b62a5ee1aeaa6a2036b4bf
Erick <eagi578@gmail.com>
2025-05-27 13:49:39 -0600

    Add Metabase dashboard embedding to DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Firebase\JWT\JWT;
 
 class DashboardController extends Controller
 {
@@ -23,6 +24,18 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $METABASE_SITE_URL = "http://localhost:3000"; // o el dominio donde corre Metabase
+        $METABASE_SECRET_KEY = "19c1faba8d589bb502fcab95b9f0d83f9bbccf03b708c4090d477e8a45eedf21";
+
+        $payload = [
+            "resource" => ["dashboard" => 2], // Cambia el 2 por el ID real del dashboard
+            "params" => new \stdClass(),
+            "exp" => time() + (10 * 60)
+        ];
+
+        $token = JWT::encode($payload, $METABASE_SECRET_KEY, 'HS256');
+        $iframeUrl = $METABASE_SITE_URL . "/embed/dashboard/" . $token . "#bordered=true&titled=true";
+
+        return view('dashboard', compact('iframeUrl'));
     }
 }
