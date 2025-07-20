d821e7b4b8e1a2ee0257b39b7ee3395674a633e8
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-07-19 23:48:49 -0600

    feat: add logout and navbar only for auth users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,29 +14,42 @@
 <body class="d-flex flex-column" style="min-height:100vh">
 
     <div class="flex-grow-1 d-flex flex-column">
+        @auth    
+            {{-- --------------- Navbar --------------- --}}
+            <nav class="custom-navbar">
+                <div class="d-flex align-items-center gap-3 w-100">
+                    {{-- Logo --}}
+                    <a href="{{ route('dashboard.index') }}" class="navbar-logo">
+                        <img src="{{ asset('images/logo-sagrada-familia.png') }}" alt="Logo" height="60">
+                    </a>
 
-        {{-- --------------- Navbar --------------- --}}
-        <nav class="custom-navbar">
-            <div class="d-flex align-items-center gap-3 w-100">
-                {{-- Logo --}}
-                <a href="{{ route('dashboard.index') }}" class="navbar-logo">
-                    <img src="{{ asset('images/logo-sagrada-familia.png') }}" alt="Logo" height="60">
-                </a>
+                    {{-- Contenedor del menú + botón user --}}
+                    <div class="nav-bubble d-flex flex-grow-1 align-items-center justify-content-end">
+                        {{-- Menú principal --}}
+                        <div class="navbar-bg">
+                            {!! \App\Navigation\Navbar::render() !!}
+                        </div>
 
-                {{-- Contenedor del menú + botón user --}}
-                <div class="nav-bubble d-flex flex-grow-1 align-items-center justify-content-end">
-                    {{-- Menú principal --}}
-                    <div class="navbar-bg">
-                        {!! \App\Navigation\Navbar::render() !!}
+                        <div class="user-dropdown-wrapper dropdown ms-2">
+                            <button class="btn btn-user nav-user-bg" data-bs-toggle="dropdown" type="button">
+                                {{ strtoupper(substr(Auth::user()->name, 0, 1)) }}
+                            </button>
+
+                            <ul class="dropdown-menu dropdown-menu-end shadow-sm mt-2">
+                                <li>
+                                    <a class="dropdown-item text-danger" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        <i class="fa-solid fa-right-from-bracket me-1"></i> Cerrar sesión
+                                    </a>
+                                    <form id="logout-form" method="POST" action="{{ route('logout') }}" class="d-none">
+                                        @csrf
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-
-                    {{-- Botón de perfil (por ahora vacío) --}}
-                    <button class="btn btn-user nav-user-bg ms-2" type="button">
-                        <i class="fa-regular fa-user fa-lg"></i>
-                    </button>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        @endauth
 
         {{-- --------------- Contenido --------------- --}}
         <main id="app" class="flex-grow-1">
