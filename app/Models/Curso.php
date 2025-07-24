42032ccc9887ed23e6ab82dbe0ccd21db3ed3c77
Erick <eagi578@gmail.com>
2025-07-24 13:41:16 -0600

    add fillable properties to Curso model for mass assignment

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
-    //
+    protected $fillable = [
+        'nombre',
+        'created_at',
+        'updated_at',
+    ];
 }
