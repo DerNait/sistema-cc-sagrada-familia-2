9126c33217c15a21e2908aa1f5b1341d440d416a
FabianMoraeles <fabimoradav2004@gmail.com>
2025-07-25 03:18:38 -0600

    CRUD Secciones, Implementacion de tabla y vinculacion con base de datos

--- app/Http/Controllers/Catalogs/SeccionesController.php
+++ app/Http/Controllers/Catalogs/SeccionesController.php
@@ -1,9 +1,12 @@
 <?php
 
+
 namespace App\Http\Controllers\Catalogs;
 
 use App\Http\Controllers\Crud\CrudControllerBase;
 use App\Models\Seccion;
+use App\Models\User;
+use App\Models\Grado;
 use Illuminate\Http\Request;
 
 class SeccionesController extends CrudControllerBase
@@ -16,8 +19,21 @@ class SeccionesController extends CrudControllerBase
             ->label('ID')
             ->readonly();
 
+        $this->column('maestro_guia_id')
+            ->label('Maestro guía')
+            ->type('relation')
+            ->rules(['required'])
+            ->options(User::pluck('name', 'id')->toArray());
+
+        $this->column('grado_id')
+            ->label('Grado')
+            ->type('relation')
+            ->rules(['required'])
+            ->options(Grado::pluck('nombre', 'id')->toArray());
+
         $this->column('seccion')
-            ->label('Sección');
+            ->label('Sección')
+            ->rules(['required', 'string', 'max:255']);
 
 
         $this->syncAbilities('secciones');
