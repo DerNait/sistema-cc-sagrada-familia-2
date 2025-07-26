d77d30ebaa5db3840393929bc59ea1b13117bbef
Erick <eagi578@gmail.com>
2025-07-25 18:04:52 -0600

    add Card component for displaying content in a card layout

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card shadow-sm h-100" style="cursor: pointer;" @click="$emit('click')">
+    <img 
+      src="https://cdn-icons-png.flaticon.com/512/8832/8832880.png" 
+      class="card-img-top" 
+      alt="Icono libro abierto"
+      style="object-fit: contain; height: 200px; padding: 20px;"
+    />
+    <div class="card-body d-flex flex-column">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
