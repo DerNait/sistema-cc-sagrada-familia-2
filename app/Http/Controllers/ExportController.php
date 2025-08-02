4fb4754af456c5c16828eb182761f429963b1717
Nery2004 <josemolinatics@gmail.com>
2025-08-01 17:13:47 -0700

    Fix change exportController

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
-use App\Exports\EmpleadosExport; // Asegúrate de importar la clase EmpleadosExport
+use App\Exports\EmpleadosExport;
 use App\Exports\ProductoExport;
 class ExportController extends Controller
 {
@@ -12,12 +12,11 @@ class ExportController extends Controller
     {
       switch ($request->tipo){  
         case 1:
-            return Excel::download(new EmpleadosExport, 'empleados.xlsx');// Obtener los empleados desde la base de datos y exportarlos a un archivo Excel
+            return Excel::download(new EmpleadosExport, 'empleados.csv');// Obtener los empleados desde la base de datos y exportarlos a un archivo csv
         case 2:
-            return Excel::download(new ProductoExport, 'productos.xlsx');
+            return Excel::download(new ProductoExport, 'productos.csv');
         default:
             return response()->json(['error'=>'Tipo de exportacion no valido'], 400);
       }
     }
 }
-    
