3a580c10f4b3c58cb7f5057405e55e6dff2d2aac
Erick <eagi578@gmail.com>
2025-08-02 21:59:18 -0600

    feat: add Filtro component for improved filtering functionality

---
+++ resources/js/components/Filtro.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="filtro-container">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .filtro-container {
+        margin-bottom: 1rem;
+    }
+</style>
