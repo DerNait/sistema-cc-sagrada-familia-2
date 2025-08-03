200977e3d69b4b122f2def55146ed19bdd9c60fa
Erick <eagi578@gmail.com>
2025-08-02 21:59:47 -0600

    refactor: update estudiante-cursos-index to include search functionality and improve course display

--- resources/js/components/estudiante-cursos-index.vue
+++ resources/js/components/estudiante-cursos-index.vue
@@ -1,26 +1,44 @@
-<script setup>
-import Card from './Card.vue'
-
-const props = defineProps({
-  cursos: {
-    type: Array,
-    default: () => []
-  }
-})
-
-const verDetalle = (cursoId) => {
-  window.location.href = `/cursos/${cursoId}`;
-}
-</script>
-
 <template>
   <div class="container py-4">
     <h2 class="mb-4">Mis Cursos</h2>
 
-    <div class="row g-3">
-      <div class="col-12 col-md-6 col-lg-4" v-for="curso in cursos" :key="curso.id">
-        <Card :curso="curso" @click="verDetalle(curso.id)" />
+    <Filtro>
+      <div class="position-relative w-100">
+        <input
+          v-model="busqueda"
+          class="form-control ps-5"
+          placeholder="Buscar curso..."
+        />
+        <i class="fas fa-search position-absolute top-50 start-0 translate-middle-y ms-3 text-muted"></i>
+      </div>
+    </Filtro>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div
+        v-for="curso in cursosFiltrados"
+        :key="curso.id"
+        class="col"
+      >
+        <Card :curso="curso" />
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import Filtro from '@/components/Filtro.vue'
+import Card from '@/components/Card.vue'
+
+const props = defineProps({
+  cursos: Array,
+})
+
+const busqueda = ref('')
+
+const cursosFiltrados = computed(() =>
+  props.cursos.filter((curso) =>
+    curso.nombre.toLowerCase().includes(busqueda.value.toLowerCase())
+  )
+)
+</script>
