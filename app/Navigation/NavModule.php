a9199b751024b8c1291d40e930b9821d7bef2783
Nery2004 <josemolinatics@gmail.com>
2025-08-04 22:36:12 -0700

    Add export method

--- app/Navigation/NavModule.php
+++ app/Navigation/NavModule.php
@@ -5,6 +5,7 @@ class NavModule
 {
     public const CRUD  = ['create', 'read', 'update', 'delete'];
     public const READ  = ['read'];
+    public const EXPORT  = ['read', 'export'];
     public const NONE  = [];
 
     public string  $label;
