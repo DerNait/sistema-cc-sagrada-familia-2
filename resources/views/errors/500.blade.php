6e320dd0bd83a3a24d20c283420ab22fccde109e
Nery2004 <josemolinatics@gmail.com>
2025-08-19 15:35:20 -0700

    Add error 500 layout

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends('errors.layout')
+
+@section('title', '500 - Not Found')
+@section('logo', asset('images/logo_.jpg'))
+@section('error_code', '500')
+@section('error_message', 'Not Found')
+@section('button_text', 'Regresar')
+@section('back_url', url()->previous() ?: url('/'))
+@section('bg_image', asset('images/fondo_error.png'))
