7139c0bf5fd60d15f53ea17fcdc233747ce9dd01
Albu231311 <alb231311@uvg.edu.gt>
2025-08-19 22:51:59 -0600

    feat: add Chart component with dynamic options and series

---
+++ resources/js/components/Chart.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <apexchart
+      :type="type"
+      :height="height"
+      :options="chartOptions"
+      :series="series"
+    />
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+  name: "Chart",
+  components: {
+    apexchart: VueApexCharts,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Gráfico",
+    },
+    type: {
+      type: String,
+      default: "bar",
+    },
+    height: {
+      type: [Number, String],
+      default: 350,
+    },
+    categories: {
+      type: Array,
+      default: () => [],
+    },
+    series: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    chartOptions() {
+      return {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: this.categories,
+        },
+        title: {
+          text: this.title,
+          align: "center",
+        },
+      };
+    },
+  },
+};
+</script>
