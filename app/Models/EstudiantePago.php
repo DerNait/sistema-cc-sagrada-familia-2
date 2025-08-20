f04bf6b55cc3243e46625b526313e0d5acfe2fbf
Erick <eagi578@gmail.com>
2025-08-20 14:33:20 -0600

    feat: create EstudiantePago model with relationships and fillable fields

---
+++ app/Models/EstudiantePago.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EstudiantePago extends Model
+{
+    // Nombre de la tabla
+    protected $table = 'estudiante_pagos';
+
+    // Campos asignables en masa
+    protected $fillable = [
+        'grado_precio_id',
+        'estudiante_id',
+        'tipo_pago_id',
+        'monto_pagado',
+        'meses_pagados',
+        'periodo_inicio',
+        'periodo_fin',
+        'tipo_estado_id'
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relaciones
+    |--------------------------------------------------------------------------
+    */
+
+    public function estudiante()
+    {
+        return $this->belongsTo(Estudiante::class);
+    }
+
+    public function gradoPrecio()
+    {
+        return $this->belongsTo(GradoPrecio::class);
+    }
+
+    public function tipoPago()
+    {
+        return $this->belongsTo(TipoPago::class);
+    }
+
+    public function tipoEstado()
+    {
+        return $this->belongsTo(TipoEstado::class);
+    }
+}
