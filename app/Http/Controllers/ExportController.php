4569c218d8b6ab0b1d91eb34f24dcb0dac04e119
Nery2004 <josemolinatics@gmail.com>
2025-08-21 17:39:20 -0700

    Add third case for Calificaciones Export

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\EmpleadosExport;
 use App\Exports\ProductoExport;
+// use App\Exports\CalificacionesExport;
 class ExportController extends Controller
 {
     public function export(Request $request)
@@ -15,6 +16,8 @@ class ExportController extends Controller
             return Excel::download(new EmpleadosExport, 'empleados.csv');// Obtener los empleados desde la base de datos y exportarlos a un archivo csv
         case 2:
             return Excel::download(new ProductoExport, 'productos.csv');
+        // case 3:
+        //     return Excel::download(new CalificacionesExport, 'calificaciones.csv');
         default:
             return response()->json(['error'=>'Tipo de exportacion no valido'], 400);
       }
