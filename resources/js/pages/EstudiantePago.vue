26341380c7bc4cc605d74d970f805c147b8a375d
Jose Galindo (Home) <kevin@softlogic.com.gt>
2025-09-10 12:23:10 -0600

    feat: add a better template for estudiante pago

---
+++ resources/js/pages/EstudiantePago.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="center-container">
+    <h3 class="p-3 fw-bold m-0">Pagos</h3>
+    <div class="filters-container px-3 py-2 d-flex justify-content-evenly align-items-end gap-2"></div>
+	<FileUpload></FileUpload>
+  </div>
+</template>
+
+<script setup>
+import FileUpload from '../components/FileUpload.vue';
+
+</script>
